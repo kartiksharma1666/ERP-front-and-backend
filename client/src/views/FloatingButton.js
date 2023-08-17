bed294e1b1be9f43a0271dde31e2f5998ca2d17e
costatian <dacostaobiriyeboah@gmail.com>
2023-08-17 15:36:22 +0000

    floating buttons update

--- client/src/views/FloatingButton.js
+++ client/src/views/FloatingButton.js
@@ -1,26 +1,31 @@
-import React from 'react'
-import {MdAdd, MdOutlineCreateNewFolder, MdMenu} from 'react-icons/md'
-import {AiOutlineEdit, AiTwotoneDelete} from 'react-icons/ai'
+import React, { useState } from 'react';
+import { MdAdd } from 'react-icons/md';
+import { BiUserPlus } from 'react-icons/bi';
 
- const FloatingButton = () => {
-    
-    const handleFabClick = () => {
-        document.querySelector('.box').classList.toggle('box-active');
-        document.querySelector('.fab').classList.toggle('fab-active');
-      };
-   
-    return (
-        <div>
-        <div className='fab'>
-            <button onClick={handleFabClick} className='fabbtn'><MdAdd /></button>
+const FloatingButton = () => {
+  const [isBoxActive, setIsBoxActive] = useState(false);
+
+  const handleFabClick = () => {
+    setIsBoxActive(!isBoxActive);
+  };
+
+  return (
+    <div>
+      <div className='fab'>
+        <button onClick={handleFabClick} className='fabbtn'>
+          <MdAdd />
+        </button>
+      </div>
+      <div className={`box ${isBoxActive ? 'box-active' : ''}`}>
+        <div className='d-flex'>
+          <span className={`add-customer-span ${isBoxActive ? 'show' : ''}`}>Add Customer</span>
+          <button className='floatbtn item4'>
+            <BiUserPlus />
+          </button>
         </div>
-        <div className='box'>
-            <button className='floatbtn item1'><AiOutlineEdit /></button>
-            <button className='floatbtn item2'><MdOutlineCreateNewFolder /></button>
-            <button className='floatbtn item3'><AiTwotoneDelete /></button>
-            <button className='floatbtn item4'><MdMenu /></button>
-        </div>
-        </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
+
 export default FloatingButton;
